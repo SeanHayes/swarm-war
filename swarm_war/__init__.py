4689220a89decb7eccdadda62fb265a0ec90274e
Sean Hayes <sean@seanhayes.name>
2011-08-11 22:32:16 -0400

    bumped version #

--- swarm_war/__init__.py
+++ swarm_war/__init__.py
@@ -1,5 +1,5 @@
 "A Facebook game engine written with Django."
 
-VERSION = (1, 0, 0, 'b')
+VERSION = (1, 0, 0, 'b', '1')
 
-__version__ = "".join([".".join(map(str, VERSION[0:3])), "".join(VERSION[3:])])
+__version__ = "".join([".".join(map(str, VERSION[:3])), "".join(VERSION[3:])])
